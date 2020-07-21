057d632f5f1f7fadc743733d3463f572835d7cb6
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-07-21 00:16:24 -0400

    skymarshal: behaviour: encode expiration in access token
    concourse/concourse#5851
    
    There's still 20 bytes of entropy, but the access token now has an
    additional 8 bytes for the unix timestamp of the expiration of the
    token. This is so we can set an expiration on the cookies set by
    skymarshal.
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -1673,7 +1673,7 @@ func (cmd *RunCommand) constructAuthHandler(
 	return token.StoreAccessToken(
 		logger.Session("dex-server"),
 		dexServer,
-		token.NewGenerator(),
+		token.Factory{},
 		token.NewClaimsParser(),
 		accessTokenFactory,
 	), nil
@@ -1710,6 +1710,7 @@ func (cmd *RunCommand) constructLoginHandler(
 	skyServer, err := skyserver.NewSkyServer(&skyserver.SkyConfig{
 		Logger:          logger.Session("sky"),
 		TokenMiddleware: middleware,
+		TokenParser:     token.Factory{},
 		OAuthConfig:     oauth2Config,
 		HTTPClient:      httpClient,
 	})
